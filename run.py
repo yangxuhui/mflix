1b2f2ff47a4e07d5ef2c2758c5a4cc85e7e2c3a5
yangxuhui <yangxuhui1992@163.com>
2019-02-16 15:58:48 +0000

    rename dotini_unix to .ini

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     app.config['MFLIX_DB_URI'] = config['PROD']['MFLIX_DB_URI']
     app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
 
-    app.run()
+    app.run(host='0.0.0.0', port=8888)
